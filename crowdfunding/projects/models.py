180234d640cfb3e60e4ffaee5544ceb2a7e7ce98
lauschof <schofieldlaura@outlook.com>
2024-09-27 21:14:17 +0800

    added model relations

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -8,3 +8,12 @@ class Project(models.Model):
     is_open = models.BooleanField()
     date_created = models.DateField(auto_now_add=True)
 
+class Pledge(models.Model):
+    amount = models.IntegerField()
+    comment = models.CharField(max_length=200)
+    anonymous = models.BooleanField()
+    project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
