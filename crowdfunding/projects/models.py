2c776f9e186344bea2210998ee528cd0f770787e
lauschof <schofieldlaura@outlook.com>
2024-10-03 20:20:25 +0800

    authentication added

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
@@ -7,6 +8,11 @@ class Project(models.Model):
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateField(auto_now_add=True)
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='owned_projects'
+    )
 
 class Pledge(models.Model):
     amount = models.IntegerField()
@@ -14,6 +20,12 @@ class Pledge(models.Model):
     anonymous = models.BooleanField()
     project = models.ForeignKey(
         'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
+
+    supporter = models.ForeignKey(
+        get_user_model(),
         on_delete=models.CASCADE,
         related_name='pledges'
     )
